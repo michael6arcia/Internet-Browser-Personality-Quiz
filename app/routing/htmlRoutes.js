641592d269d461ec3338604d60f3f8219daa1b00
mchlgrca <mgarcia0898@gmail.com>
2019-06-08 14:17:04 -0700

    html routes added to htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,13 @@
-var server = require("../../server.js");
+var path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+
+};

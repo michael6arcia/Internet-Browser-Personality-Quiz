8787c3a745dabfe1b4f0125b7b32d88feb172f79
mchlgrca <mgarcia0898@gmail.com>
2019-06-08 13:35:24 -0700

    added new require routes to server.js

--- server.js
+++ server.js
@@ -9,4 +9,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
+
 module.exports = server;
